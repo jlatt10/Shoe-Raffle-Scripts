913af81932c2758c10ad1a95ace0c2eb7269c8ee
jlatt10 <43796957+jlatt10@users.noreply.github.com>
2018-10-02 17:45:01 -0400

    Update needsupply.py

--- needsupply.py
+++ needsupply.py
@@ -1,22 +1,19 @@
-import requests
-from random import getrandbits
-url = 'http://needsupply.com/yeezy'
+url = 'https://thewebster.us/shop/the-10-nike-x-off-white-blazer/'
 
 headers = {'User-Agent':
            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'}
 
-
-# CHANGE the fields as the comments say  
+  
 def main(limit):
     for i in range(1, limit):
         num = getrandbits(40)
-        name = 'your name{}'.format(num) # put your name here, don't remove the {}
-        email = 'your_email+{}@gmail.com'.format(num) # CHANGE YOUR_EMAIL to your email prefix. don't change the +{} after.
+        name = 'Jack Lattman{}'.format(num) # put your name here, don't remove the {}
+        email = 'jlatt10+{}@gmail.com'.format(num) # CHANGE YOUR_EMAIL to your email prefix. don't change the +{} after.
         payload = {
             'action': 'contest',
-            'name': name, 
-            'email': email,
-            'size': '9.5', # change your size
+            'name': Jack Lattman, 
+            'email': jlatt10@gmail.com,
+            'size': '6.5', # change your size
             'group[]': 'Mens'
         }
         resp = requests.post(url, data=payload, headers=headers)
